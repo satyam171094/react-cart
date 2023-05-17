ce248855275deb306aab01a177de5959403d0fd1
yourGitHubusernamesatyam171094 <satis.jais112@gmail.com>
2023-05-17 23:39:18 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {Toaster} from "react-hot-toast"
+import Cart from "./component/Cart";
+import Header from "./component/Header";
+import Home from "./component/Home";
+
+import "./styles/app.scss"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/cart" element={<Cart />}/>
+    </Routes>
+    < Toaster />
+   </Router>
   );
 }
 
